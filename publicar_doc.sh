a565a11adbd3fca1e4f7130dde4f9ea83ab74a1a
Ricardo Pérez <ricpelo@gmail.com>
2017-02-17 19:08:10 +0100

    Corrige publicar_doc.sh para que la guía sea la portada de la documentación

--- publicar_doc.sh
+++ publicar_doc.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -rf docs/
-vendor/bin/apidoc api commands,controllers,assets,models,views docs --pageTitle="VideoYii API" --guide=. --guidePrefix=guide- --interactive=0
-vendor/bin/apidoc guide guia docs --pageTitle="Guía de VideoYii" --guidePrefix=guide- --apiDocs=. --interactive=0
+vendor/bin/apidoc api commands,controllers,assets,models,views docs/api --pageTitle="VideoYii API" --guide=.. --guidePrefix= --interactive=0
+vendor/bin/apidoc guide guia docs --pageTitle="Guía de VideoYii" --guidePrefix= --apiDocs=api --interactive=0
+ln -sf README.html docs/index.html
 touch docs/.nojekyll
