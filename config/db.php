d3eb68392fc5505cbc139dfd9f06a6a353c04a85
Ricardo Pérez <ricpelo@gmail.com>
2017-03-25 17:11:24 +0100

    Refactorización de config/db.php

--- config/db.php
+++ config/db.php
@@ -1,28 +1,27 @@
 <?php
 
-$url = getenv('DATABASE_URL');
-
-if ($url !== false) {
-    $matches = parse_url($url);
-    $host = $matches['host'];
-    $port = $matches['port'];
-    $dbname = substr($matches['path'], 1);
-    $username = $matches['user'];
-    $password = $matches['pass'];
-
-    return [
-        'class' => 'yii\db\Connection',
-        'dsn' => "pgsql:host=$host;port=$port;dbname=$dbname",
-        'username' => $username,
-        'password' => $password,
-        'charset' => 'utf8',
-    ];
+if (($url = getenv('DATABASE_URL')) !== false) {
+    // Configuración para Heroku:
+    $config = parse_url($url);
+    $host = $config['host'];
+    $port = $config['port'];
+    $dbname = substr($config['path'], 1);
+    $username = $config['user'];
+    $password = $config['pass'];
+} else {
+    // Configuración para entorno local:
+    $host = 'localhost';
+    $port = '5432';
+    $dbname = 'videoyii';
+    $username = 'videoyii';
+    $password = 'videoyii';
 }
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=videoyii',
-    'username' => 'videoyii',
-    'password' => 'videoyii',
+    'dsn' => "pgsql:host=$host;port=$port;dbname=$dbname",
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 ];
+
