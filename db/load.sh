bb4321c300b40c4b1bb2d7537ade159f331e4b1e
Ricardo Pérez <ricpelo@gmail.com>
2017-09-24 16:12:09 +0200

    Mejora los tests en Travis-CI con PostgreSQL

--- db/load.sh
+++ db/load.sh
@@ -2,6 +2,6 @@
 
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
-psql -U proyecto proyecto < $DIR/proyecto.sql
+[ "$1" != "test" ] && psql -U proyecto proyecto < $DIR/proyecto.sql
 psql -U proyecto proyecto_test < $DIR/proyecto.sql
 
