a60e1743bafe6355f92dd08b0776991d20d8c397
Ricardo Pérez <ricpelo@gmail.com>
2017-09-24 10:26:42 +0200

    Corrige creación de BD y creación de BD de test

--- db/create.sh
+++ db/create.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-sudo -u postgres dropuser proyecto
-sudo -u postgres dropdb proyecto
+sudo -u postgres dropuser --if-exists proyecto
+sudo -u postgres dropdb --if-exists proyecto
 sudo -u postgres psql -c "create user proyecto password 'proyecto' superuser;"
 sudo -u postgres createdb -O proyecto proyecto
+sudo -u postgres createdb -O proyecto proyecto_test
 
--- db/load.sh
+++ db/load.sh
@@ -3,4 +3,5 @@
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
 psql -U proyecto proyecto < $DIR/proyecto.sql
+psql -U proyecto proyecto_test < $DIR/proyecto.sql
 
