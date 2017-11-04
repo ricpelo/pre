87bfedb58abd6bb462e5dc6e45f9cf72d845fad1
Ricardo Pérez <ricpelo@gmail.com>
2017-11-04 17:18:20 +0100

    Generaliza publicar_doc.sh

--- publicar_doc.sh
+++ publicar_doc.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 rm -rf docs/
-vendor/bin/apidoc api commands,controllers,assets,models,views docs/api --pageTitle="VideoYii API" --guide=.. --guidePrefix= --interactive=0
-vendor/bin/apidoc guide guia docs --pageTitle="Guía de VideoYii" --guidePrefix= --apiDocs=./api --interactive=0
+vendor/bin/apidoc api commands,controllers,assets,models,views docs/api --pageTitle="API del proyecto" --guide=.. --guidePrefix= --interactive=0
+vendor/bin/apidoc guide guia docs --pageTitle="Guía del proyecto" --guidePrefix= --apiDocs=./api --interactive=0
 ln -sf README.html docs/index.html
 touch docs/.nojekyll
+
