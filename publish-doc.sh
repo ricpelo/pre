6392f6e7af36905c801e518f3024531aebea2985
Ricardo Pérez <ricpelo@gmail.com>
2017-11-07 09:27:50 +0100

    Usa --exclude en publish-doc.sh

--- publish-doc.sh
+++ publish-doc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -rf docs/
-vendor/bin/apidoc api commands,components,controllers,assets,models,views docs/api --pageTitle="API del proyecto" --guide=.. --guidePrefix= --interactive=0
+vendor/bin/apidoc api . docs/api --pageTitle="API del proyecto" --guide=.. --guidePrefix= --exclude="docs,vendor,tests" --interactive=0
 vendor/bin/apidoc guide guia docs --pageTitle="Guía del proyecto" --guidePrefix= --apiDocs=./api --interactive=0
 ln -sf README.html docs/index.html
 touch docs/.nojekyll
